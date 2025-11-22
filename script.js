c6c014ba35731f1162fbe7b6d235de3591c33d50
kxoen <phonekaew8467@gmail.com>
2025-11-22 21:03:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const music = document.getElementById("bgmusic");
 
-// เล่นเพลงเมื่อกดปุ่ม NO
+// เล่นเพลงเมื่อกด NO
 noBtn.addEventListener("click", () => {
   music.play();
 });
 
-// ปุ่ม No หนีเมาส์
+// ปุ่ม NO หนี
 noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * window.innerWidth * 0.8;
-  const y = Math.random() * window.innerHeight * 0.8;
+  const container = document.querySelector('.btn-box');
+  const containerRect = container.getBoundingClientRect();
+  
+  const btnWidth = noBtn.offsetWidth;
+  const btnHeight = noBtn.offsetHeight;
+
+  const maxX = containerRect.width - btnWidth;
+  const maxY = containerRect.height - btnHeight;
+
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
 });
 
 // ปุ่ม YES
 yesBtn.addEventListener("click", () => {
-  alert("ดีใจที่สุดเลย 💖 ผมรักพี่นะคับบ!");
+  alert("ดีใจที่สุดเลย 💖 ผมรักพี่นัทที่สุดเลยย");
 });
